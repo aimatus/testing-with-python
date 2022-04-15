7be4c84623878571f52f80e89418ba8d149ff2a4
Abraham Matus <aimatusco@gmail.com>
2022-04-14 23:15:37 -0500

    adds empty test cases

--- 02_fizzbuzz_test_cases/fizzbuzz_test.py
+++ 02_fizzbuzz_test_cases/fizzbuzz_test.py
@@ -7,3 +7,18 @@ def test_returns_same_number():
   result = play(number)
 
   assert result == number
+
+def test_returns_fizz_when_multiple_of_3():
+  pass
+
+def test_returns_buzz_when_multiple_of_5():
+  pass
+
+def test_returns_fizzbuzz_when_multiple_of_3_and_5():
+  pass
+
+def test_returns_error_message_when_number_more_than_100():
+  pass
+
+def test_throws_exception_when_playing_with_negative_numbers():
+  pass
