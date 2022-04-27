a6410aeda92885c921b495ca9263f6693d0dd0cc
Abraham Matus <aimatusco@gmail.com>
2022-04-27 16:45:33 -0500

    adds dummy logic

---
+++ 03_payment_app_test_cases/app/email/email.py
@@ -0,0 +1,3 @@
+def send_email(to_email: str, subject: str, body: str):
+  print(f'Sending email to {to_email}')
+  raise Exception('Could not connect to email server')
--- 03_payment_app_test_cases/external_services/paypal.py
+++ 03_payment_app_test_cases/external_services/paypal.py
@@ -1,2 +1,4 @@
-def make_payment():
-  pass
+def charge_payment(email: str, amount: str):
+  print('Connecting to Paypal')
+  print(f'Charging ${amount} to ${email}')
+  raise Exception(f'PayPal Error! Could not charge amount to user ${email}. API key required')
