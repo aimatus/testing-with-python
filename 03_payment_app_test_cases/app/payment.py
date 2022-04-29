8350b44f625b02530ff15527754a4273c5d6a7d1
Abraham Matus <aimatusco@gmail.com>
2022-04-29 14:49:09 -0500

    cleans up implementation and tests

--- 03_payment_app_test_cases/app/payment.py
+++ 03_payment_app_test_cases/app/payment.py
@@ -2,11 +2,4 @@ import external_libraries.paypal as paypal
 import app.dao.product_dao as dao
 
 def charge_product(id: int, email: str):
-  product = dao.get_product_by_id(id)
-
-  paypal.charge_payment(email, product.price)
-
-  return {
-    'product': product.__dict__,
-    'payment_status': 'ok'
-  }
+  pass
--- 03_payment_app_test_cases/app/payment_test.py
+++ 03_payment_app_test_cases/app/payment_test.py
@@ -2,39 +2,12 @@ import pytest
 
 from app.payment import charge_product
 
-# Add description to logic
-def test_should_return_product_information_and_payment_status_ok(mocker):
-  # Arrange
-  mocker.patch('external_libraries.paypal.charge_payment')
-  product_id = 1
-  email_dummy = 'mytestemail@test.com'
-  expected_result = {
-    'product': {
-      'id': 1,
-      'name': 'Nintendo Switch Oled',
-      'price': 50000
-    },
-    'payment_status': 'ok'
-  }
-
-  # Act
-  result = charge_product(id=product_id, email=email_dummy)
-
-  # Assert
-  assert result == expected_result
+def test_should_return_product_information_and_payment_status_ok():
+  pass
 
 
-def test_should_charge_paypal_for_product_price(mocker):
-  # Arrange
-  charge_payment_spy = mocker.patch('external_libraries.paypal.charge_payment')
-  product_id = 1
-  email_dummy = 'mytestemail@test.com'
-
-  # Act
-  result = charge_product(id=product_id, email=email_dummy)
-
-  # Assert
-  charge_payment_spy.assert_called()
+def test_should_charge_paypal_for_product_price():
+  pass
 
 
 def test_should_email_user_with_product_details():
@@ -42,4 +15,4 @@ def test_should_email_user_with_product_details():
 
 
 def test_should_use_proper_item_from_database():
-  product_id = 89156
+  pass
